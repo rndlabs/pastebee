2e87ef1186e22f0c3f87538d105f8205477d12e2
sig <dan@1up.digital>
2020-08-19 08:26:03 +0100

    adds axios and some functionality

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,22 @@
-var app6 = new Vue({
-  el: '#app-6',
-  data: {
-    message: 'Hello Vue!'
-  }
-})
+let app = new Vue({
+    el: '#app',
+    data: {
+      pasteText: 'Hello Vue!',
+      hash: ''
+    },
+    methods: {
+        clearPasteText: function(){
+            this.pasteText = '';
+        },
+        createPaste: async function(){
+            let formData = new FormData();
+            formData.append('pastebee.com.txt', this.pasteText);
+            let response = await axios.post('http://localhost:8083/files', formData, {
+                headers: {
+                  'Content-Type': 'text/plain'
+                }
+            });
+            this.hash = response.data.reference
+        }
+    }
+});
