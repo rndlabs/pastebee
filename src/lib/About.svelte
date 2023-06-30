367a76856d2c7ed094ab53bc5a97853fbd842a45
mfw78 <mfw78@protonmail.com>
2023-06-30 10:06:48 +0000

    feat: components

---
+++ src/lib/About.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let hideHandler: () => void;
+</script>
+
+<div id="about-page" class="overlay bounce-in-top">
+	<button id="close" on:click={hideHandler}>X</button>
+	<div class="va-outer">
+		<div class="va-inner">
+			<div class="container">
+				<div id="about-content">
+					<!-- svelte-ignore a11y-missing-content -->
+					<a id="about-1up" href="https://1up.digital" target="_blank" />
+					<!-- svelte-ignore a11y-missing-content -->
+					<a id="about-swarm" href="https://www.ethswarm.org/" target="_blank" />
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ src/lib/Footer.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let showTermsHandler: () => void;
+</script>
+
+<div id="footer">
+	<a class="mobile-only" id="powered-by-swarm" href="https://www.ethswarm.org/" target="_blank" />
+</div>
+<div id="terms" on:click={showTermsHandler}>Terms &amp; Conditions</div>
