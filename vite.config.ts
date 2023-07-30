1def7fbef64c4a98559befea089e3ad8cef8022e
mfw78 <mfw78@protonmail.com>
2023-07-30 17:07:12 +0000

    chore: repo config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), wasm()],
 });
